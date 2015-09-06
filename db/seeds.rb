516a8040bee3d04050bcfe0b2be7c62ae62f1b32
bhopek <bhopek@gmail.com>
2015-09-06 23:52:34 +0200

    Add sample packages in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+  FactoryGirl.create(:package, message: "message#{i}", bike_id: i)
+end
