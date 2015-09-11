1a8eaa2cbaa11e845f7359d5bca3f9388f19ae34
bhopek <bhopek@gmail.com>
2015-09-12 00:11:11 +0200

    Fix tests - last is nil when there are no packages

--- spec/jobs/get_packages_job_spec.rb
+++ spec/jobs/get_packages_job_spec.rb
@@ -9,11 +9,11 @@ describe GetPackagesJob, type: :job do
     subject { -> { described_class.perform_now } }
 
     context 'when there is valid package' do
-      let(:packages) { [{ bike_id: 9, message: 'abc' }] }
+      let(:packages) { [{ bike_id: 91, message: 'abc' }] }
 
       it { is_expected.to change { Package.count }.by(1) }
-      it { is_expected.to change { Package.last.bike_id }.to(9) }
-      it { is_expected.to change { Package.last.message }.to('abc') }
+      it { is_expected.to change { Package.all.collect(&:bike_id) }.to([91]) }
+      it { is_expected.to change { Package.all.collect(&:message) }.to(['abc']) }
     end
   end
 end
