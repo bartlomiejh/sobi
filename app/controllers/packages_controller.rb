d44d1ac01a4e52e9fef0b4801195694051bdc4c2
bhopek <bhopek@gmail.com>
2015-09-14 01:08:12 +0200

    Pull packages from the queue every 5 seconds

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,6 +2,8 @@ class PackagesController < ApplicationController
   # GET /packages
   # GET /packages.json
   def index
+    # @todo bhopek: move job execution to scheduler/background
+    GetPackagesJob.perform_now
     @packages = Package.all.order('created_at DESC')
   end
 end
