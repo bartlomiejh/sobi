af4dd859f286a8c9390693bd248c5b10c62408b7
bhopek <bhopek@gmail.com>
2015-09-14 01:01:25 +0200

    Reorder packages on the list

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,6 +2,6 @@ class PackagesController < ApplicationController
   # GET /packages
   # GET /packages.json
   def index
-    @packages = Package.all
+    @packages = Package.all.order('created_at DESC')
   end
 end
