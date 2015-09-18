cadb0c03ea866a22e8aeffbe5f1d2b741c0015d3
bhopek <bhopek@gmail.com>
2015-09-18 02:00:34 +0200

    Add review remark

--- app/jobs/get_packages_job.rb
+++ app/jobs/get_packages_job.rb
@@ -8,6 +8,7 @@ class GetPackagesJob < ActiveJob::Base
 
   def perform
     collect_until_nil { @redis.rpop RedisConfig.queue }.each do |package_params|
+      # @review bhopek: validate json contract
       p = Package.create JSON.parse(package_params).slice('bike_id', 'message')
       # @review bhopek: add fancy logging - rollbar, snugbug
       p.valid? ? p.save! : Rails.logger.error("Params: #{package_params}, errors: #{p.errors.full_messages}")
